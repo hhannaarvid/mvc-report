9f5dcc84ed48acf0e1fac19818f99c68d5525d27
hhannaarvid <176487087+hhannaarvid@users.noreply.github.com>
2025-04-13 14:00:03 +0200

    file structure changes

---
+++ src/Controller/DiceGameController.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use App\Dice\Dice;
+
+class DiceGameController extends AbstractController
+{
+    #[Route("/game/pig", name: "pig_start")]
+    public function home(): Response
+    {
+        return $this->render('pig/homepig.html.twig');
+    }
+
+    #[Route("/game/pig/test/roll", name: "test_roll_dice")]
+    public function testRollDice(): Response
+    {
+        $die = new Dice();
+
+        $data = [
+            "dice" => $die->roll(),
+            "diceString" => $die->getAsString(),
+        ];
+
+        return $this->render('pig/test/roll.html.twig', $data);
+    }
+
+    #[Route("/game/pig/test/roll/{num<\d+>}", name: "test_roll_num_dices")]
+    public function testRollDices(int $num): Response
+    {
+        if ($num > 99) {
+            throw new \Exception("Can not roll more than 99 dices!");
+        }
+
+        $diceRoll = [];
+        for ($i = 1; $i <= $num; $i++) {
+            $die = new Dice();
+            $die->roll();
+            $diceRoll[] = $die->getAsString();
+        }
+
+        $data = [
+            "num_dices" => count($diceRoll),
+            "diceRoll" => $diceRoll,
+        ];
+
+        return $this->render('pig/test/roll_many.html.twig', $data);
+    }
+}
