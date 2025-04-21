d2e479c8ba54bb644206949b5f2b8bd98fa03246
hhannaarvid <176487087+hhannaarvid@users.noreply.github.com>
2025-04-21 16:21:26 +0200

    add report questions

---
+++ tools/phpstan/vendor/composer/installed.php
@@ -0,0 +1,32 @@
+<?php return array(
+    'root' => array(
+        'name' => '__root__',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => 'f163a1f2d0d9f87f69e48df78cfe5608151c75f3',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        '__root__' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => 'f163a1f2d0d9f87f69e48df78cfe5608151c75f3',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+        'phpstan/phpstan' => array(
+            'pretty_version' => '2.1.12',
+            'version' => '2.1.12.0',
+            'reference' => '96dde49e967c0c22812bcfa7bda4ff82c09f3b0c',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../phpstan/phpstan',
+            'aliases' => array(),
+            'dev_requirement' => true,
+        ),
+    ),
+);
